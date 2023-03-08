a15602492d800eab9854c827fe8f027cc9a39545
xiphoidbox <alebermea10@outlook.com>
2023-03-08 16:15:35 -0600

    path generator
    almost done with path generator.

--- MechanicsDescent/Assets/scripts/GridManager.cs
+++ MechanicsDescent/Assets/scripts/GridManager.cs
@@ -6,26 +6,29 @@ public class GridManager : MonoBehaviour
 {
     public int gridWidth = 16;
     public int gridHeight = 8;
+    public int minPathLength = 30;
     public GameObject pathTile;
     private PathGenerator pathGenerator;
 
     // Start is called before the first frame update
     void Start()
     {
-        // create a new PathGenerator with specified grid width and height
         pathGenerator = new PathGenerator(gridWidth, gridHeight);
-        // generate a path using the PathGenerator and store it in a list of Vector2Int
         List<Vector2Int> pathCells = pathGenerator.GeneratePath();
-        // for each Vector2Int in the pathCells list, instantiate a pathTile object at that position
-        for (int i = 0; i < 500; i++)
+        int pathSize = pathCells.Count;
+        while (pathSize < minPathLength) 
         {
-
+            pathCells = pathGenerator.GeneratePath();
+            pathSize = pathCells.Count;
+        }
+        foreach (Vector2Int pathCell in pathCells)
+        {
+            Instantiate(pathTile, new Vector2(pathCell.x, pathCell.y), Quaternion.identity);;
         }
     }
 
-    // Update is called once per frame
     void Update()
     {
-        // empty, no update required for this script
+
     }
 }
--- MechanicsDescent/Assets/scripts/PathGenerator.cs
+++ MechanicsDescent/Assets/scripts/PathGenerator.cs
@@ -7,26 +7,76 @@ public class PathGenerator
     private int width, height;
     private List<Vector2Int> pathCells;
 
-    // Constructor that takes the width and height of the grid as parameters
     public PathGenerator(int width, int height)
     {
         this.width = width;
         this.height = height;
     }
 
-    // Method that generates a list of Vector2Int representing the path cells
     public List<Vector2Int> GeneratePath()
     {
-        // Initialize the list of path cells
         pathCells = new List<Vector2Int>();
-        // Determine the vertical position of the path cells
         int y = (int)(height / 2);
-        // Iterate through the width of the grid, adding a path cell for each x value
-        for (int x = 0; x < width; x++)
+        int x = 0;
+        while (x < width)
         {
             pathCells.Add(new Vector2Int(x, y));
+            bool validMove = false;
+            while(!validMove)
+            {
+                int move = Random.Range(0, 3);
+                if (move == 0 || x % 2 == 0 || x > (width - 2))
+                {
+                    x++;
+                    validMove = true;
+                }
+                else if (move == 1 && CellIsFree(x, y + 1) && y < (height - 2))
+                {
+                    y++;
+                    validMove = true;
+                }
+                else if (move == 2 && CellIsFree(x, y - 1) && y > 2)
+                {
+                    y--;
+                    validMove = true;
+                }
+            }
         }
-        // Return the list of path cells
         return pathCells;
+    }
+
+    private bool CellIsFree(int x, int y)
+    {
+        return !pathCells.Contains(new Vector2Int(x, y));
+    }
+
+    private bool CellIsntFree(int x, int y)
+    {
+        return pathCells.Contains(new Vector2Int(x, y));
+    }
+
+    private int getCellNeighbourValue(int x, int y)
+    {
+        int returnValue = 0;
+        if (CellIsntFree(x, y - 1))
+        {
+            returnValue += 1;
+        }
+
+        if (CellIsntFree(x, y + 1))
+        {
+            returnValue += 8;
+        }
+
+        if (CellIsntFree(x - 1, y))
+        {
+            returnValue += 2;
+        }
+
+        if (CellIsntFree(x + 1, y))
+        {
+            returnValue += 4;
+        }
+        return returnValue;
     }
 }
